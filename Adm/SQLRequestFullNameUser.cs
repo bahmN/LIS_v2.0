6492f6099f484dbe26c5d2eeecd560fb8380ae7c
ivn <iwanaev23@gmail.com>
2020-11-25 14:55:57 +0300

    sql-request class

---
+++ Adm/SQLRequestFullNameUser.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace LIS.Adm
+{
+    class SQLRequestFullNameUser
+    {
+        public static string fullNameUser(string idUser)
+        {
+            MySqlCommand cFullNameUser = new MySqlCommand("SELECT ФИО FROM Пользователь WHERE `ID пользователя`= '"+idUser+"'", frmAuthorization.connection);
+            object userFullNameObj = cFullNameUser.ExecuteScalar();
+            string userFullName = userFullNameObj.ToString();
+            return userFullName;
+        }
+    }
+}
